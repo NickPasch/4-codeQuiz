c851952032dbd96e56147e500a75eddc40b0b7b7
Nick Pasch <paschnick100@gmail.com>
2020-11-11 16:47:01 -0700

    functionality fully there, application meets all requirements, needs comments

--- Assets/script.js
+++ Assets/script.js
@@ -3,14 +3,19 @@ var playAgain = document.querySelector("#play")
 var theQuiz = document.querySelector("#quizInt")
 var theStart = document.querySelector("#startInt")
 var theEnd = document.querySelector("#endInt");
+var theView = document.querySelector("#viewInt")
+var scoreBut = document.querySelector("#saveScore")
 var but0 = document.querySelector("#ans0")
 var but1 = document.querySelector("#ans1")
 var but2 = document.querySelector("#ans2")
 var but3 = document.querySelector("#ans3")
 var quest = document.querySelector("#quest")
-
-var count = 40;
-
+var theScore = document.querySelector("#score")
+var scoreboard = document.querySelector("#scoreboard")
+var initials = document.querySelector("#initials")
+var count = 10;
+var score = 0;
+var currentQuestion = 0; 
 var questions = [
     {
         question: "What goes at the end of every javascript statement?",
@@ -53,17 +58,40 @@ var questions = [
         answer: "Grey",
     }];
 
-var score = 0;
-var currentQuestion = 0; 
-
+if(localStorage.getItem("score")){
+    leaderBoard = JSON.parse(localStorage.getItem("score"))
+} else {
+    var leaderBoard = [];
+}
 
 function viewScores(){
+    if(initials.value !== initials){
+        theView.setAttribute("class", "View Score")
+        theEnd.setAttribute("class", "End Interface hidden")
+        storeIt();
 
+        
+    }  
+}
+
+function storeIt(){
+    var newScoreTable = initials.value + " - " + count
+    leaderBoard.push(newScoreTable)
+    localStorage.setItem("score", JSON.stringify(leaderBoard))
+    for(i=0; i<leaderBoard.length; i++){
+        var newScore = document.createElement("li");
+        var forText = document.createElement("a");
+        newScore.appendChild(forText);
+        scoreboard.appendChild(newScore);
+        forText.innerHTML = leaderBoard[i];
+    }
 }
 
 function gameOver(){
+    console.log(count)
     theEnd.setAttribute("class", "End Interface")
     theQuiz.setAttribute("class", "Quiz Interface hidden")
+    theScore.textContent = " Your Score: " + count ;
     currentQuestion = 0;
     console.log(currentQuestion);
 }
@@ -76,77 +104,49 @@ function startGame(){
     var interval = setInterval(function(){
         document.getElementById('count').textContent = count;
         count--;
-        if (count === 0){
-          clearInterval(interval);
-          document.getElementById('count').innerHTML='Done';
-          gameOver();
-        }
-        if (currentQuestion === questions.length){
+        if (count <= 0){
+            clearInterval(interval);
+            gameOver();
+            youLost();
+            count = count;
+            console.log(count)
+        }else if(currentQuestion === questions.length){
+            clearInterval(interval);
+            count = count + 2;
             gameOver();
         }
       
     }, 1000)
-    // calls first question, starts timer, hides menu 
     nextQuestion();
 }
 
 function nextQuestion(){
-    
     quest.textContent = questions[currentQuestion].question
     but0.textContent = questions[currentQuestion].choices[0]
     but1.textContent = questions[currentQuestion].choices[1]
     but2.textContent = questions[currentQuestion].choices[2]
     but3.textContent = questions[currentQuestion].choices[3] 
-
 }
 
-startButton.addEventListener("click", startGame)
-
-
-document.addEventListener("click", function(event){
+const answerClick = function(event){
     var answer = questions[currentQuestion].answer
+    console.log("targetText1: ", event.target.textContent)
+    console.log("correctAnswer1: ", answer)
     if(event.target.textContent === answer){
         currentQuestion++;
         console.log(currentQuestion);
         nextQuestion();   
-    }
-    if(event.target.textContent !== answer){
+    }else if(event.target.textContent !== answer){
         count = count - 5;
-
     }
-})
+}
 
-// document.addEventListener("click", function(event){
-//     var notAnswer = questions[currentQuestion].answer
-//     if(event.target.textContent !== notAnswer){
-//         count = count - 5;
+startButton.addEventListener("click", startGame)
+scoreBut.addEventListener("click", viewScores)
+but0.addEventListener("click", answerClick)
+but1.addEventListener("click", answerClick)
+but2.addEventListener("click", answerClick)
+but3.addEventListener("click", answerClick)
 
-//     }
-// })
-
-
-function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
-  
-    // While there remain elements to shuffle...
-    while (0 !== currentIndex) {
-  
-      // Pick a remaining element...
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex -= 1;
-  
-      // And swap it with the current element.
-      temporaryValue = array[currentIndex];
-      array[currentIndex] = array[randomIndex];
-      array[randomIndex] = temporaryValue;
-    }
-  
-    return array;
-  }
-  
-  // Used like so
-  var arr = [2, 11, 37, 42];
-  shuffle(arr);
-  console.log(arr);
 
 
